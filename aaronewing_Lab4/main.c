48dc18a1cdd59061efe500e04b017208250b1b00
Aaron Ewing <aaronewing@nnu.edu>
2016-11-26 01:28:49 -0700

    and....it stable

--- aaronewing_Lab4/main.c
+++ aaronewing_Lab4/main.c
@@ -6,45 +6,44 @@
 
 uint8_t RXData = 0x00;			// global data
 uint8_t TXData = 0xFF;			// transmit data
-bool new_Data_Flag = 0;			// data to convert flag
-bool transmit_Data_Flag = 0;	// transmit data flag
+uint8_t state = 0;
 bool LED_Flag = 0;				// LED flag
 
 enum UART_States { RX_Data, convert_Data, TX_Data } UART_State;
 void TickFct_UART() {
 	switch(UART_State) {   // Transitions
   	case RX_Data:
-  		if (!new_Data_Flag && !transmit_Data_Flag) {
+  		if (state == 0) {
   			UART_State = RX_Data;
   		}
-  		if (new_Data_Flag && !transmit_Data_Flag) {
+  		if (state == 1) {
   		  	UART_State = convert_Data;
   		}
-  		if (!new_Data_Flag && transmit_Data_Flag) {
+  		if (state == 2) {
   			UART_State = TX_Data;
   		}
   		break;
 
   	case convert_Data:
-		if (!new_Data_Flag && !transmit_Data_Flag) {
+		if (state == 0) {
 			UART_State = RX_Data;
 		}
-		if (new_Data_Flag && !transmit_Data_Flag) {
+		if (state == 1) {
 			UART_State = convert_Data;
 		}
-		if (!new_Data_Flag && transmit_Data_Flag) {
+		if (state == 2) {
 			UART_State = TX_Data;
 		}
 		break;
 
 	case TX_Data:
-		if (!new_Data_Flag && !transmit_Data_Flag) {
+		if (state == 0) {
 			UART_State = RX_Data;
 		}
-		if (new_Data_Flag && !transmit_Data_Flag) {
+		if (state == 1) {
 		  	UART_State = convert_Data;
 		}
-		if (!new_Data_Flag && transmit_Data_Flag) {
+		if (state == 2) {
 			UART_State = TX_Data;
 		}
 		break;
@@ -55,8 +54,7 @@ void TickFct_UART() {
 
 	switch (UART_State) {   // State actions
 		case RX_Data:
-			new_Data_Flag = 0;
-			transmit_Data_Flag = 0;
+			// do nothing
 			break;
 
 		case convert_Data:
@@ -66,14 +64,12 @@ void TickFct_UART() {
 			} else {
 				TXData = RXData;						// do nothing to input
 			}
-			new_Data_Flag = 0;
-			transmit_Data_Flag = 1;
+			state = 2;
 			break;
 
 		case TX_Data:
 			write_UART(TXData, 0);						// send TXData through UART
-			new_Data_Flag = 0;
-			transmit_Data_Flag = 0;
+			state = 0;
 			LED_Flag = 0;								// turn off LED
 			break;
 
@@ -136,8 +132,7 @@ __interrupt void USCI_A0_ISR(void) {
     while (!(UCA0IFG & UCTXIFG));           // USCI_A0 TX buffer ready?
     RXData = UCA0RXBUF;                  	// TX -> RXData
 
-    new_Data_Flag = 1;
-    transmit_Data_Flag = 0;
+    state = 1;
     LED_Flag = 1;
     break;
   case 4:break;                             // Vector 4 - TXIFG
